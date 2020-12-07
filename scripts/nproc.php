7a63160f0f6edf47f10072e1dd7034ba5a8fb10f
Fabien Villepinte <fabien.villepinte@gmail.com>
2020-12-07 11:25:20 +0100

    Save one call to getCPU()

--- scripts/nproc.php
+++ scripts/nproc.php
@@ -6,8 +6,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $parser = (new \Linfo\Linfo())->getParser();
 
-if ($parser !== null && method_exists($parser, 'getCPU') && is_array($parser->getCPU())) {
-    return (string) max(1, count($parser->getCPU()));
+if ($parser !== null && method_exists($parser, 'getCPU') && is_array($CPU = $parser->getCPU())) {
+    return (string) max(1, count($CPU));
 }
 
 return '1';
